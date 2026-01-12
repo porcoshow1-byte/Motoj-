56a0f5e0e3d5576ec17d6b67baa69f26e3397226
porcoshow1-byte <porcoshow1-byte@users.noreply.github.com>
2026-01-12 02:13:28 -0300

    chore: setup capacitor for android native app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.motoja.app',
+  appName: 'MotoJa',
+  webDir: 'dist'
+};
+
+export default config;
